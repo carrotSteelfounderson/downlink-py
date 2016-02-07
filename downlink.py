023cbdd8ddd61b5375f96f5d4722bd56ee328d69
J.H.Gabriel <jhg8@kent.ac.uk>
2016-02-07 00:53:08 +0000

    Add User class, add getters and tests for other classes

--- downlink.py
+++ downlink.py
@@ -1,19 +1,74 @@
-# Downlink v0.00, written by John Gabriel, 07/02/16
+# Downlink, written by John Gabriel, 07/02/16
 
-class Comp(object): #A computer. Every computer in the game can (security notwithstanding) be controlled.
-	def __init__(self, name, address=-1):
+class Comp: #A computer. Every computer in the game can (security notwithstanding) be controlled.
+	def __init__(self, name, address):
 		self.name = name #The name of the system. Not used to connect to it! That's the address.
 		self.address = address #Defaults to -1 for now. (I can set appropiate addesses if I want - otherwise this will later assign a random IP-like address.)
-		self.root = Dir("") #For the purposes of the simulation, all systems have one and only one filesystem. This is just a blank-named directory.
+		self.drive = Dir("") #For the purposes of the simulation, all systems have one and only one 'drive'. This is just a blank-named directory.
 		self.users = [] #By default, all comps will have a root account with some hefty protection. Randomly-generated comps will get randomly-generated users with looser security.
+		
+	@property
+	def name(self):
+		return self.name
+	
+	@property
+	def users(self):
+		return self.users
+		
+	@property
+	def users(self):
+		return self.users
+		
+	@property
+	def drive(self):
+		return self.drive
+		
+class User: #An account on a computer. For sake of avoiding potential headaches, one instance of a user should only ever be used by one comp! (Unless... hm.)
+	def __init__(self, name, password=-1):
+		self.name = name #The username, seen by player.
+		self.password = password #Used for logging in. Will be randomly generated unless it's specified.
 
-class Dir(object): #A folder/directory.
+class Dir: #A folder/directory.
 	def __init__(self, name):
 		self.name = name #Used as foldername, seen by player.
 		self.files = [] #Folders can store other folders!
 		self.dirs = [] #Folders have files in them.
+		
+	@property
+	def name(self):
+		return self.name
+		
+	@property
+	def files(self):
+		return self.files
+		
+	@property
+	def dirs(self):
+		return self.dirs
+		
+	def addFile(self, fileToAdd):
+		self.files.append(fileToAdd)
+		
+	def addDir(self, dirToAdd):
+		self.dirs.append(dirToAdd)
+		
+	def listFiles(self):
+		for nextFile in self.files:
+			print nextFile.name
 
-class File(object): #A file. Contains stuff.
-	def __init__(self, name, contents=""):
+class File: #A file. Contains stuff.
+	def __init__(self, name, extension="", contents=""):
 		self.name = name #Used as filename, seen by player.
-		self.contents #What's in the file, and what will be shown when the player cats the file. Useful for world-building.
+		self.extension = extension #Filetype, seen by played. Can be blank.
+		self.contents = contents #What's in the file, and what will be shown when the player cats the file. Useful for world-building.
+	
+	@property
+	def name(self):
+		return self.name
+
+#del folder(fileIndex)
+
+testComp = Comp("localhost", "127.0.0.1")
+testFile = File("test", "txt", "Lorem ipsum...")
+testComp.drive.addFile(testFile)
+testComp.drive.listFiles()
